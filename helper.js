0d618e12b8b705563674ccd7e7bf83122069f64b
liujiusheng <891599396@qq.com>
2019-08-17 12:28:43 +0800

    上传代码

---
+++ helper.js
@@ -0,0 +1,42 @@
+function getJSON(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300 && xhr.response) {
+            callback(xhr.response);
+        }
+    };
+    xhr.send();
+}
+
+
+
+//用promise
+function getData(url){
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+      });
+}
+
+
+//post请求
+function postJSON(url, data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300 && xhr.response) {
+            callback(xhr.response);
+        }
+    };
+    xhr.send(data);
+}
+
+
